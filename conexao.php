538b7971f7f8576c6b9c8c47699cf1f03e9f06f4
Randerson-Abdon <66569225+Randerson-Abdon@users.noreply.github.com>
2021-10-13 19:58:06 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -6,9 +6,9 @@ date_default_timezone_set('America/Sao_Paulo');
 //CONEXAO LOCAL
 //OBJETO QUE SE MANTEM, CONSTANTE COM DADOS PARA CONEXÃO AO BD
 define('HOST', 'localhost'); //servidor
-define('USUARIO', 'provider'); //usuario do banco de dados
-define('SENHA', 'Jp?20061965'); //senha do banco de dados
-define('BD', 'santaizabel-pa'); //do banco de dados
+define('USUARIO', ''); //usuario do banco de dados
+define('SENHA', ''); //senha do banco de dados
+define('BD', ''); //do banco de dados
 
 //VARIAVEL PARA GUARDAR A EXECUÇÃO DA CONEXÃO
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, BD) or die('Não Conectou');
